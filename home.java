08f3f437b2c2aeff9242e9a65c93ac2e96202e6c
AnikaRose <anikaroserimi@gmail.com>
2026-05-19 12:18:25 +0600

    Added first Java file

---
+++ home.java
@@ -0,0 +1,11 @@
+
+public class home {
+    public int windows;
+    public int doors;
+    
+    public void view(){
+        System.out.println( windows + " windows");
+        System.out.println( doors + " doors");
+    }
+    
+}
