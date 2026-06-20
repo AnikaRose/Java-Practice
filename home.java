10231b841ff6f9d7a6cca76959add822669c5db0
Anika Rose Rimi <anikaroserimi@gmail.com>
2026-06-20 23:57:55 +0600

    Remove blank line in home.java
    Remove unnecessary blank line in home class

--- home.java
+++ home.java
@@ -1,7 +1,6 @@
 public class home {
     public int windows;
     public int doors;
-    
     public void view(){
         System.out.println( windows + " windows");
         System.out.println( doors + " doors");
