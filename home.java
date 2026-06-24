820ad2bc8282129c5dd1a1ce8169dce19965bf13
Anika Rose Rimi <anikaroserimi@gmail.com>
2026-06-25 01:54:00 +0600

    Update view method to improve output messages

--- home.java
+++ home.java
@@ -2,7 +2,7 @@ public class home {
     public int windows;
     public int doors;
     public void view(){
-        System.out.println( windows + " windows");
-        System.out.println( doors + " doors");
+        System.out.println( windows + " windows are there");
+        System.out.println( doors + " doors are there");   
     }    
 }
