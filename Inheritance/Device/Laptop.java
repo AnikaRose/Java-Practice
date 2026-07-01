62fa30e8844d6c0b220aba4d8bafcce2e07794ea
Anika Rose Rimi <anikaroserimi@gmail.com>
2026-07-02 00:28:30 +0600

    Improve equals and toString methods in Laptop
    Refactor equals and toString methods for Laptop class.

--- Inheritance/Device/Laptop.java
+++ Inheritance/Device/Laptop.java
@@ -5,15 +5,21 @@ public class Laptop extends Device{
         super(brand,price);
         this.ram = ram;
     }
+    
     @Override
     public boolean equals(Object obj){
         if(!super.equals(obj)){
             return false;
         }    
             Laptop other = (Laptop) obj;
-            return this.ram == other.ram;   
+            return this.ram == other.ram;
+        
     }
+    
+    
     public String toString(){
-        return super.toString() + "Laptop [ RAM: " + ram + "GB]";
-    } 
+        return super.toString() + " -> Laptop [RAM: " + ram + "GB]";
+    }
+    
+    
 }
