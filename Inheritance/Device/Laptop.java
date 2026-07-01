221a87d7979370a514d988c2f594a7b580cb336d
Anika Rose Rimi <anikaroserimi@gmail.com>
2026-07-02 00:16:07 +0600

    Refactor equals method and format Laptop class
    Refactor equals method for Laptop class and adjust formatting.

--- Inheritance/Device/Laptop.java
+++ Inheritance/Device/Laptop.java
@@ -5,21 +5,15 @@ public class Laptop extends Device{
         super(brand,price);
         this.ram = ram;
     }
-    
     @Override
     public boolean equals(Object obj){
         if(!super.equals(obj)){
             return false;
         }    
             Laptop other = (Laptop) obj;
-            return this.ram == other.ram;
-        
+            return this.ram == other.ram;   
     }
-    
-    
     public String toString(){
         return super.toString() + "Laptop [ RAM: " + ram + "GB]";
-    }
-    
-    
+    } 
 }
