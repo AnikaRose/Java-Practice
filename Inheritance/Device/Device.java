fb704327eec3a7bc147cda845af24563ad2eac54
Anika Rose Rimi <anikaroserimi@gmail.com>
2026-07-02 00:15:18 +0600

    Refactor equals method and clean up formatting

--- Inheritance/Device/Device.java
+++ Inheritance/Device/Device.java
@@ -18,15 +18,9 @@ public class Device {
     Device other = (Device) obj;
     return Double.compare(this.price , other.price) == 0 &&
             (this.brand != null ? this.brand.equals(other.brand) : other.brand == null);
-    
-    
-    }
-    
-    
+    } 
     @Override
     public String toString(){
-        return "Brand: " + brand + "\nPrice: " + price ;
-        
+        return "Brand: " + brand + "\nPrice: " + price ;    
     }
-
 }
