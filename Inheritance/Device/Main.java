1b2410f1225f563ed1dc2135570465b956d0e7f1
Anika Rose Rimi <anikaroserimi@gmail.com>
2026-07-02 00:23:04 +0600

    Rename device variables for consistency

--- Inheritance/Device/Main.java
+++ Inheritance/Device/Main.java
@@ -1,3 +1,7 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package Inheritance.Device;
 
 /**
@@ -7,9 +11,9 @@ package Inheritance.Device;
 public class Main {
     public static void main(String[] args) {
         // Creating generic devices
-        Device genericDevice1 = new Device("Sony", 299.99);
-        Device genericDevice2 = new Device("Sony", 299.99);
-        Device genericDevice3 = new Device("Apple", 999.99);
+        Device Device1 = new Device("Sony", 299.99);
+        Device Device2 = new Device("Sony", 299.99);
+        Device Device3 = new Device("Apple", 999.99);
 
         // Creating Laptops (Inheritance in action)
         Laptop laptop1 = new Laptop("Dell", 800.00, 16);
@@ -18,14 +22,14 @@ public class Main {
 
         // --- Testing Equals Method ---
         System.out.println("Generic Devices:");
-        System.out.println(genericDevice1+" "+genericDevice2+" "+genericDevice3);
+        System.out.println(Device1+" "+Device2+" "+Device3);
         System.out.println("Laptops:");
         System.out.println(laptop1+" "+laptop2+" "+laptop3);
         System.out.println(laptop3);
         
         System.out.println("--- Generic Device Comparisons ---");
-        System.out.println("Is device1 equal to device2? " + genericDevice1.equals(genericDevice2)); // True
-        System.out.println("Is device1 equal to device3? " + genericDevice1.equals(genericDevice3)); // False
+        System.out.println("Is device1 equal to device2? " + Device1.equals(Device2)); // True
+        System.out.println("Is device1 equal to device3? " + Device1.equals(Device3)); // False
 
         System.out.println("\n--- Laptop Comparisons (Inheritance & Overriding) ---");
         // True because brand, price, AND ram match
@@ -36,6 +40,6 @@ public class Main {
 
         // False because they are different types altogether
         System.out.println("\n--- Cross-Class Comparison ---");
-        System.out.println("Is laptop1 equal to genericDevice1? " + laptop1.equals(genericDevice1)); 
+        System.out.println("Is laptop1 equal to Device1? " + laptop1.equals(Device1)); 
     }
 }
