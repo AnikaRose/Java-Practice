040902b16008425a731e064d138e3583983278a9
Anika Rose Rimi <anikaroserimi@gmail.com>
2026-07-02 00:14:51 +0600

    Fix formatting and consistency in Device.java

--- Inheritance/Device/Device.java
+++ Inheritance/Device/Device.java
@@ -17,10 +17,16 @@ public class Device {
     }
     Device other = (Device) obj;
     return Double.compare(this.price , other.price) == 0 &&
-            (this.brand != null ? this.brand.equals(other.brand) : other.brand == null);   
+            (this.brand != null ? this.brand.equals(other.brand) : other.brand == null);
+    
+    
     }
+    
+    
     @Override
     public String toString(){
-        return "Brand: " + brand + "\nPrice: " + price ;      
+        return "Brand: " + brand + "\nPrice: " + price ;
+        
     }
+
 }
