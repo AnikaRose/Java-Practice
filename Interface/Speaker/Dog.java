7bfc0e67fc9d2881f8b9b3a90df690610d5186ac
Anika Rose Rimi <anikaroserimi@gmail.com>
2026-07-04 23:08:07 +0600

    Fix output format in Dog's speak method

--- Interface/Speaker/Dog.java
+++ Interface/Speaker/Dog.java
@@ -5,7 +5,7 @@ public class Dog implements Speaker {
         this.name = name;
     }
     public void speak(){
-        System.out.println("Ghew ");
+        System.out.println("Ghew");
     }
     public String toString(){
       return "Dog: " + name;
